68de8287da91616232f2b138fe692c0fd5b8b04c
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2023-10-19 22:20:37 -0300

    feat: create route list professionals

---
+++ src/app/existProfessional/[userId]/list/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params: { userId } }: { params: { userId: string } }) {
+  const { searchParams } = new URL(request.url);
+
+  console.log({ userId });
+  
+  if (!userId) {
+      return {
+          status: 400,
+      body: {
+        message: "Missing userId",
+      },
+    };
+}
+
+const professional = await prisma.prestador.findMany({
+    where: {
+        id_user: userId
+    },
+    select:{
+        id: true
+    }
+});
+
+  return new NextResponse(JSON.stringify(professional), { status: 200 });
+}
