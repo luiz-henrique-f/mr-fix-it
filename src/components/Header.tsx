101cd48cfa05b60a3c3c0193631478915436841a
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2023-10-31 19:31:00 -0300

    fix: ordenation menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ import { Prestador } from "@prisma/client";
 
 interface ProfessionalProps {
     professional: Prestador;
-  }
+}
 
 
 const Header = () => {
@@ -25,18 +25,18 @@ const Header = () => {
     // const theme = document.documentElement.classList.contains("dark") ? 'dark' : 'light';
     // const currentTheme = document.documentElement.classList.contains("dark") ? 'dark' : 'light';
 
-    function light()  {
+    function light() {
         // localStorage.theme = 'light'
         setTheme('light')
         document.documentElement.classList.remove('dark')
         // console.log(localStorage.theme);
     }
-    
-    function dark()  {
+
+    function dark() {
         setTheme("dark")
         document.documentElement.classList.add('dark')
         // console.log(localStorage.theme);
-      }
+    }
 
     const [menuIsOpen, setMenuIsOpen] = React.useState(false)
     const { status, data } = useSession();
@@ -66,7 +66,7 @@ const Header = () => {
         <div className='px-[5%] py-0 h-[85px] mx-auto flex justify-between items-center shadow-2xl bg-whiteBG dark:bg-darkBG'>
             <div className="flex items-center justify-start h-full w-full">
                 <Link href="/" className="inline-flex items-center justify-between">
-                    
+
                     <Image
                         src="/Logo_3.0_roxo.png"
                         width={50}
@@ -108,7 +108,7 @@ const Header = () => {
             {status === "unauthenticated" && (
                 <div className="flex items-center gap-3 relative">
 
-                    {theme == 'light' ? <BiSolidMoon size={16} onClick={() => currentTheme == "dark"? light(): dark()} className="cursor-pointer text-black "/> : <BsSunFill size={16} onClick={() => currentTheme == "dark"? light(): dark()} className="cursor-pointer text-white "/>}
+                    {theme == 'light' ? <BiSolidMoon size={16} onClick={() => currentTheme == "dark" ? light() : dark()} className="cursor-pointer text-black " /> : <BsSunFill size={16} onClick={() => currentTheme == "dark" ? light() : dark()} className="cursor-pointer text-white " />}
 
                     <Link href={'/login'}>
                         <button className="flex text-black dark:text-white items-center justify-center py-2 px-4 gap-2 text-sm uppercase bg-transparent font-semibold border-2 border-solid border-black/50 dark:border-white/70 rounded-md hover:bg-primary hover:border-primary dark:hover:border-primary hover:text-white transition-all duration-[0.3s] ease-[ease-in-out] hover:transition-all hover:duration-[0.3s] hover:ease-[ease-in-out]">
@@ -119,23 +119,23 @@ const Header = () => {
                 </div>
             )}
 
-            {status === "authenticated" && data.user &&(
+            {status === "authenticated" && data.user && (
                 <div className="flex items-center gap-3 border-grayLighter border border-solid p-2 px-3 rounded-full relative">
-                    <AiOutlineMenu onClick={handleMenuClick} className="cursor-pointer text-3xl 2sm:text-2xl md:text-xl"/>
+                    <AiOutlineMenu onClick={handleMenuClick} className="cursor-pointer text-3xl 2sm:text-2xl md:text-xl" />
 
-                    {theme == 'light' ? 
-                        <BiSolidMoon 
-                            onClick={() => currentTheme == "dark"? light(): dark()} 
+                    {theme == 'light' ?
+                        <BiSolidMoon
+                            onClick={() => currentTheme == "dark" ? light() : dark()}
                             className="cursor-pointer text-3xl 2sm:text-2xl md:text-xl"
                         />
-                        : 
-                        <BsSunFill 
-                            onClick={() => currentTheme == "dark"? light(): dark()} 
+                        :
+                        <BsSunFill
+                            onClick={() => currentTheme == "dark" ? light() : dark()}
                             className="cursor-pointer text-3xl 2sm:text-2xl md:text-xl"
                         />
                     }
-                    
-                    <Image height={35} width={35} src={data?.user?.image!} alt={data?.user?.name!} className="rounded-full shadow-md"/>
+
+                    <Image height={35} width={35} src={data?.user?.image!} alt={data?.user?.name!} className="rounded-full shadow-md" />
 
                     {menuIsOpen && (
                         <div className="z-50 absolute top-14 left-0 w-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center dark:bg-zinc-800">
@@ -145,26 +145,27 @@ const Header = () => {
                                 </button>
                             </Link>
 
-                            <button 
-                                className="text-primary text-sm font-semibold border-b-4"
-                                onClick={() => {signOut({ redirect: false }).then(() => {
+                            <Link href={`/professionals/90fd330c-51c6-4285-8082-71c4075539c4`}>
+                                <button className="text-primary text-sm font-semibold border-b-4" onClick={hidennMenu}>
+                                    Meu Perfil
+                                </button>
+                            </Link>
+
+                            <button
+                                className="text-primary text-sm font-semibold"
+                                onClick={() => {
+                                    signOut({ redirect: false }).then(() => {
                                         router.push("/"); // Redirect to the dashboard page after signing out
                                     });
                                 }}>
                                 Logout
                             </button>
-                            
-                            <Link href={`/professionals/90fd330c-51c6-4285-8082-71c4075539c4`}>
-                                <button className="text-primary text-sm font-semibold" onClick={hidennMenu}>
-                                    Meu Perfil
-                                </button>
-                            </Link>
                         </div>
                     )}
-                    
+
                 </div>
 
-            )} 
+            )}
         </div>
     )
 }
