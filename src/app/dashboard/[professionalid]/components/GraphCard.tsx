ae1d6b92f8e80a77e732fdee21c7affc0081970c
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2023-12-03 20:31:52 -0300

    fix: error in grahCard

--- src/app/dashboard/[professionalid]/components/GraphCard.tsx
+++ src/app/dashboard/[professionalid]/components/GraphCard.tsx
@@ -25,45 +25,45 @@ const GraphCard = (props: { itemName: string; itemName2: string; title: string; 
 
   const [chartOptions, setChartOptions] = useState({});
 
-  useEffect(() => {
-    setChartData({
-      labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-      datasets: [
-        {
-          label: [itemName2],
-          data: [3, 8, 12, 5, 2, 9, 4, 13, 6, 3, 1, 7],
-          backgroundColor: '#d33',
-          borderRadius: 8,
-        }, 
-        {
-          label: [itemName],
-          data: [19, 5, 8, 12, 9, 2, 16, 23, 13, 4, 5, 10],
-          backgroundColor: '#2a6',
-          borderRadius: 8,
-        }, 
-      ],
-    });
+  // useEffect(() => {
+  //   setChartData({
+  //     labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  //     datasets: [
+  //       {
+  //         label: [itemName2],
+  //         data: [3, 8, 12, 5, 2, 9, 4, 13, 6, 3, 1, 7],
+  //         backgroundColor: '#d33',
+  //         borderRadius: 8,
+  //       }, 
+  //       {
+  //         label: [itemName],
+  //         data: [19, 5, 8, 12, 9, 2, 16, 23, 13, 4, 5, 10],
+  //         backgroundColor: '#2a6',
+  //         borderRadius: 8,
+  //       }, 
+  //     ],
+  //   });
 
-    setChartOptions({
-      plugins: {
-          legend: {
-            position: 'top',
-            labels: {
-              color: '#aaa',
-              usePointStyle: true,
-              pointStyle: 'circle',
-            },
-          },
-          title: {
-            display: true,
-            text: [title],
-            color: '#aaa',
-          },
-      },
-      maintainAspectRatio: false,
-      responsive: true
-  });
-  }, []);
+  //   setChartOptions({
+  //     plugins: {
+  //         legend: {
+  //           position: 'top',
+  //           labels: {
+  //             color: '#aaa',
+  //             usePointStyle: true,
+  //             pointStyle: 'circle',
+  //           },
+  //         },
+  //         title: {
+  //           display: true,
+  //           text: [title],
+  //           color: '#aaa',
+  //         },
+  //     },
+  //     maintainAspectRatio: false,
+  //     responsive: true
+  // });
+  // }, []);
   
   return (
     <>
