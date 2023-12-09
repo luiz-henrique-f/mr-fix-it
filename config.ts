d0d017fbc0ab6cfc83c7df8bea9cf47fa8b1f2c8
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2023-12-09 20:42:25 -0300

    fix: bugs in mail

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    EMAIL: "luizhfernandes100@outlook.com",
+    PASSWORD: "lateraldireito2907"
+}
--- src/app/sendMail/route.ts
+++ src/app/sendMail/route.ts
@@ -1,23 +1,29 @@
+import { config } from "../../../config";
+
+
 const nodemailer = require('nodemailer')
+// const config = require('../../lib/config')
 
 export async function POST(request: Request) {
     const req = await request.json();
     
     const { email, nome, data_fim } = req;
 
+    console.log(config.EMAIL)
+
     const transporter = nodemailer.createTransport({
         host: "smtp.office365.com",
         port: 587,
         secure: false,
         auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
+            user: config.EMAIL,
+            pass: config.PASSWORD,
         },
-        from: process.env.EMAIL,
+        from: config.EMAIL,
     });
 
     await transporter.sendMail({
-        from: process.env.EMAIL, // sender address
+        from: config.EMAIL, // sender address
         to: email, // list of receivers
         subject: `Olá, ${nome}`, // Subject line
         text: `O seu plano vence no dia ${data_fim}, atualize seu plano. Equipe Mr. Fix It`, // plain text body
