8301c6771b6165cf914c4f7bb02ba93b50802e38
Luiz Madrid <luizam987@gmail.com>
2023-12-08 22:52:05 -0300

    fix: change made to the shadow and background of the platform feedback section.

--- src/app/components/FeedbackCard.tsx
+++ src/app/components/FeedbackCard.tsx
@@ -8,11 +8,11 @@ interface FeedbackProps {
 const FeedbackCard = ({ feedback }: FeedbackProps) => {
   return (
     <>
-      <div className="flex flex-col w-full max-w-sm drop-shadow-[0px_25px_30px_rgba(120,120,120,0.5)] hover:scale-105 transition-all duration-[0.3s] ease-[ease-in-out] hover:transition-all hover:duration-[0.3s] hover:ease-[ease-in-out]">
-        <div className="relative bg-white rounded-t-xl h-40 py-8 flex justify-center items-center">
+      <div className="flex flex-col w-full max-w-sm drop-shadow-[0px_25px_30px_rgba(120,120,120,0.5)] dark:drop-shadow-[0px_25px_30px_rgba(120,120,120,0.26)] hover:scale-105 transition-all duration-[0.3s] ease-[ease-in-out] hover:transition-all hover:duration-[0.3s] hover:ease-[ease-in-out]">
+        <div className="relative bg-white dark:bg-darkBGLighter rounded-t-xl h-40 py-8 flex justify-center items-center">
           <ImQuotesLeft className="absolute top-2 left-2 opacity-20 text-5xl text-grayPrimary" />
 
-          <span className="text-xl text-black font-medium px-6">{feedback.comentario}</span>
+          <span className="text-xl text-black dark:text-white font-medium dark:font-normal px-6">{feedback.comentario}</span>
         </div>
 
         <div className="flex flex-col justify-end items-center rounded-b-xl bg-gradient-to-tl from-primary to-primaryLighter py-2">
