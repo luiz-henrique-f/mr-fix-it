cd4774da09f1a322a5dd19530f4752a4ea843717
Luiz Madrid <luizam987@gmail.com>
2023-12-11 21:11:50 -0300

    feat: profilecards in search now has a new feature.

--- src/app/search/components/ProfileCard.tsx
+++ src/app/search/components/ProfileCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Button from '@/components/Button';
 
 import { FaMapPin } from 'react-icons/fa'
 import { Prestador } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
-import Link from 'next/link';
+
 import { BsStarFill } from 'react-icons/bs';
+import { IoInformationCircleOutline } from "react-icons/io5";
+
 
 interface ProfileInfoProps {
   prestador: Prestador,
@@ -24,17 +27,24 @@ const ProfileCard = ({ prestador, nota }: ProfileInfoProps) => {
 
         <div className="relative bg-white dark:bg-darkBGLighter rounded-2xl shadow-lg mx-2 mb-4 overflow-hidden">
 
-          {!nota ? (
-            <span className="absolute invisible flex items-center w-12 justify-around text-black bg-yellow-400 rounded-[4px] top-6 left-6 px-2 py-1 text-sm font-bold roll-in-blurred-right">
+          {(nota as any) >= 3 ? (
+            <div className='relative top-6 mx-3 flex justify-between items-center'>
+              <span className="flex items-center w-12 justify-around text-black bg-yellow-300 rounded-[4px] px-2 py-1 text-sm font-bold roll-in-blurred-right">
+                {nota}
+                <BsStarFill />
+              </span>
+
+              <span className='text-grayPrimary/70 group relative'>
+                <IoInformationCircleOutline className='text-xl' />
+                <p className='hidden group-hover:block absolute w-64 -top-5 right-7 p-2 text-sm italic font-medium text-justify indent-2 rounded-md bg-gray-200 after:border-l-[16px] after:border-r-[16px] after:border-b-[16px] after:border-transparent after:border-b-gray-200 after:absolute after:rotate-90 after:top-6 after:-right-5'>Profissionais com médias maiores ou iguais a três, tem suas notas expostas no canto superior de seus cards!</p>
+              </span>
+          </div>
+          ) : (
+            <span className="absolute invisible">
               {nota}
               <BsStarFill />
             </span>
-          ) :
-            (<span className="absolute flex items-center w-12 justify-around text-black bg-yellow-400 rounded-[4px] top-6 left-6 px-2 py-1 text-sm font-bold roll-in-blurred-right">
-              {nota}
-              <BsStarFill />
-            </span>)
-          }
+          )}
 
           <div className='flex flex-col items-center justify-center mb-8'>
             <div className='rounded-full p-[6px] border-2 border-primary dark:border-primaryLighter mt-10 mb-4'>
