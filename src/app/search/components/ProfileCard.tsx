491a8f5cce365024abdd1cbdd6adb7a6a2a8bf13
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-12-11 22:54:43 -0300

    feat: add avaliacoes

--- src/app/search/components/ProfileCard.tsx
+++ src/app/search/components/ProfileCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import Button from '@/components/Button';
 
 import { FaMapPin } from 'react-icons/fa'
-import { Prestador } from '@prisma/client';
+import { Comentarios_Prestador, Prestador } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
 
 import { BsStarFill } from 'react-icons/bs';
@@ -13,11 +13,17 @@ import { IoInformationCircleOutline } from "react-icons/io5";
 
 
 interface ProfileInfoProps {
-  prestador: Prestador,
+  comentarios: Comentarios_Prestador
+  prestador: Prestador
   nota: number
 };
 
-const ProfileCard = ({ prestador, nota }: ProfileInfoProps) => {
+const ProfileCard = async ({ prestador, nota }: ProfileInfoProps) => {
+  const comentarios = await prisma.comentarios_Prestador.findMany({
+    where: {
+      id_prestador: prestador.id
+    }
+  })
 
   return (
     <>
@@ -27,18 +33,24 @@ const ProfileCard = ({ prestador, nota }: ProfileInfoProps) => {
 
         <div className="relative bg-white dark:bg-darkBGLighter rounded-2xl shadow-lg mx-2 mb-4 overflow-hidden">
 
-          {(nota as any) >= 3 ? (
+          {nota ? (
             <div className='relative top-6 mx-3 flex justify-between items-center'>
-              <span className="flex items-center w-12 justify-around text-black bg-yellow-300 rounded-[4px] px-2 py-1 text-sm font-bold roll-in-blurred-right">
-                {nota}
-                <BsStarFill />
-              </span>
+              <div className='flex w-40 items-center justify-between'>
+                <span className="flex items-center w-12 justify-around text-black dark:text-white bg-yellow-300 rounded-[4px] px-2 py-1 text-sm font-bold roll-in-blurred-right">
+                  {nota}
+                  <BsStarFill />
+                </span>
+
+                <span className="text-sm text-grayPrimary">
+                  {comentarios.length} {'avaliação(ões)'}
+                </span>
+              </div>
 
               <span className='text-grayPrimary/70 group relative'>
                 <IoInformationCircleOutline className='text-xl' />
                 <p className='hidden group-hover:block absolute w-64 -top-5 right-7 p-2 text-sm italic font-medium text-justify indent-2 rounded-md bg-gray-200 after:border-l-[16px] after:border-r-[16px] after:border-b-[16px] after:border-transparent after:border-b-gray-200 after:absolute after:rotate-90 after:top-6 after:-right-5'>Profissionais com médias maiores ou iguais a três, tem suas notas expostas no canto superior de seus cards!</p>
               </span>
-          </div>
+            </div>
           ) : (
             <span className="absolute invisible">
               {nota}
